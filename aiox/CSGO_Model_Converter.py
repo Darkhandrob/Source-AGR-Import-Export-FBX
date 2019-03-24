ff32280f0d8891126e4de3a411711b628a73c880
Devo <bnn1@hotmail.de>
2019-03-24 18:12:41 +0100

    Update CSGO_Model_Converter.py

--- aiox/CSGO_Model_Converter.py
+++ aiox/CSGO_Model_Converter.py
@@ -135,6 +135,10 @@ class CSModelConverter(bpy.types.Operator):
             if i.name.find("physics") != -1:
         
                 bpy.data.objects.remove(i)
+            # Delete smd_bone_vis
+            if i.name.find("smd_bone_vis") != -1:
+        
+                bpy.data.objects.remove(i)
         # Create Directory
         NewModelPath = ModelPath.split(self.filepath)[1]
         if self.invSubfld:
