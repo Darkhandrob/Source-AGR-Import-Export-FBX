f2041a8d35f86497f741346ff29755454730a2f5
Darkhandrob <36556620+Darkhandrob@users.noreply.github.com>
2019-03-26 18:59:58 +0100

    Update __init__.py

--- aiox/__init__.py
+++ aiox/__init__.py
@@ -2,13 +2,13 @@
 # https://github.com/Darkhandrob
 # https://www.youtube.com/user/Darkhandrob
 # https://twitter.com/Darkhandrob
-# Last change: 17.03.2019
+# Last change: 26.03.2019
 
 bl_info = {
     "name": "Source AGR Importer to FBX Exporter(AIOX)",
     "category": "Import-Export",
     "author": "Darkhand",
-    "version": (1, 5, 2),
+    "version": (1, 5, 3),
     "blender": (2, 80, 0),
     "description": "Imports AGR and Exports every animation as its own FBX",
     "location": "File > Import/Export"
